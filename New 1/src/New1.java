89225a38231fcc7e53fe7bdd0ee9f3790f6855e9
DELL <DELL@DESKTOP-IF2PEG5>
2021-02-18 11:40:22 +0300

    ADD another statement

--- New 1/src/New1.java
+++ New 1/src/New1.java
@@ -10,43 +10,43 @@ System.out.println("enter the firs number");
 int a = input.nextInt();
 System.out.println("enter the socend number");
 int b = input.nextInt();
-sum(a,b);
+math(a,b);
 System.out.println("enter the firs number");
 double c = input.nextDouble();
 System.out.println("enter the socend number");
 double d = input.nextDouble();
 System.out.println("enter the third number");
 double q = input.nextDouble();
-sum2(a,b,d);
+math(a,b,c);
 System.out.println("enter the firs number");
 int f = input.nextInt();
 System.out.println("enter the socend number");
 double h = input.nextDouble();
-sum3(a,b);
+math(a,b);
 System.out.println("enter the firs number");
 double g = input.nextDouble();
 System.out.println("enter the socend number");
 int k = input.nextInt();
-sum4(a,b);
+math (a,b);
 	}
-public static int sum(int a,int b){
-	int sum=(a*b);
-	System.out.println("the factorial equals ;"+sum);
-	return sum;
+public static int math(int a,int b){
+	int math=(a*b);
+	System.out.println("the factorial equals ;"+math);
+	return math;
 }	
-	public static double sum2(double a,double b,double d){
-		double sum2=(a-b-d);
-		System.out.println("the substrahend equal ;"+sum2);
-		return sum2;
+	public static double math(double a,double b,double c){
+		double math=(a-b-c);
+		System.out.println("the substrahend equal ;"+ math);
+		return math;
 	
 }
-	public static void sum3(int a,double b){
-		double sum3=(a+b);
-		System.out.println("the sum equal ;"+sum3);
+	public static void math(int a,double b){
+		double math=(a+b);
+		System.out.println("the sum equal ;"+ math);
 		
 }
-	public static void sum4(double a,int b){
-		double sum4=(a/b);
-		System.out.println("the divisor equal ;"+sum4);	
+	public static void  math(double a,int b){
+		double  math=(a/b);
+		System.out.println("the divisor equal ;"+ math);	
 }
 }
